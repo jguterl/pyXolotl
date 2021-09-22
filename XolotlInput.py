4c21733b66db17a7b579d26883d067d422354468
jerome.guterl <guterlj@fusion.gat.com>
2021-09-22 13:52:14 -0700

    fix param_def

--- XolotlInput.py
+++ XolotlInput.py
@@ -7,7 +7,45 @@ Created on Fri Sep 10 00:23:26 2021
 """
 from SimManager2 import *
 import re
-
+param_Def = 
+{'dimensions': ' The number of dimensions for the simulation. Can be 0 (no dimension), 1, 2, 3. Note: 0 dimension cannot be run in parallel.',
+ 'gridType': ' Grid type to use along X. (default = uniform; available uniform,nonuniform,geometric,cheby,read.',
+ 'gridParam': ' At most six parameters for the grid. Alternatives: uniform -> nX hX; nonuniform -> nX; geometric -> nX ratio; cheby -> nX width. The four additional parameters are for a uniform grid in Y and Z -> nY hY nZ hZ.',
+ 'boundary': ' This option allows the user to choose the boundary conditions. The first one correspond to the left side (surface) and second one to the right (bulk), then two for Y and two for Z. 0 means mirror or periodic, 1 means free surface.',
+ 'xBCType': ' The boundary conditions to use in the X direction, mirror (default) or periodic.',
+ 'voidPortion': ' The value (in %) of the void portion at the start of the simulation.',
+ 'netParam': ' This option allows the user to define the boundaries of the network. To do so, simply write the values in order maxHe/Xe maxD maxT maxV maxI.',
+ 'networkFile': ' The HDF5 file to use for restart.',
+ 'process': ' List of all the processes to use in the simulation (reaction, diff, advec, modifiedTM, movingSurface, bursting, attenuation, resolution, heterogeneous, sink).',
+ 'burstingDepth': ' The depth (in nm) after which there is an exponential decrease in the probability of bursting (10.0 nm if nothing is specified).',
+ 'burstingFactor': ' This option allows the user to set the factor used in computing the likelihood of a bursting event.',
+ 'material': ' The material options are as follows: {W100, W110, W111, W211, Pulsed, Fuel, Fe, 800H}.',
+ 'grouping': ' The grouping parameters: the first integer is the size at which the grouping starts (HeV clusters in the PSI case, Xe in the NE case), the second is the first width of the groups (He for PSI, Xe for NE), and the third one in the second width of the groups (V for PSI).',
+ 'tempHandler': ' Temperature handler to use. (default = constant; available constant,gradient,heat,profile.',
+ 'tempParam': ' At most two parameters for temperature handler. Alternatives: constant -> temp; gradient -> surfaceTemp bulkTemp; heat -> heatFlux bulkTemp.',
+ 'tempProfileFilename': ' A temperature profile is given by the specified file, then linear interpolation is used to fit the data. NOTE: no need for tempParam here.',
+ 'flux': ' The value of the incoming flux in #/nm2/s. If the Fuel case is used it actually corresponds to the fission rate in #/nm3/s.',
+ 'fluxFile': ' A time profile for the flux is given by the specified file, then linear interpolation is used to fit the data. (NOTE: If a flux profile file is given, a constant helium flux should NOT be given).',
+ 'fluxDepthProfileFilePath': ' The path to the custom flux profile file; the default is an empty string that will use the default material associated flux handler.',
+ 'initialV': ' The initial concentration of vacancies in the material (in #/nm3).',
+ 'sputtering': ' The sputtering yield (in atoms/ion) that will be used.',
+ 'grain': ' This option allows the user to add GB in the X, Y, or Z directions. To do so, simply write the direction followed by the distance in nm, for instance: X 3.0 Z 2.5 Z 10.0 .',
+ 'zeta': ' The value of the electronic stopping power in the material (0.73 by default).',
+ 'density': ' Sets a density in nm-3 for the number of xenon per volume in a bubble for the NE case (default is 10.162795276841 nm-3 as before).',
+ 'radiusSize': ' This option allows the user to set a minimum size for the computation for the average radii, in the same order as the netParam option (default is 0).',
+ 'perfHandler': ' Which set of performance handlers to use. (default = os, available dummy,os,papi).',
+ 'vizHandler': ' Which set of handlers to use for the visualization. (default = dummy, available std,dummy).',
+ 'petscArgs': ' All the arguments that will be given to PETSc. See PETSc Options.',
+ 'rng': " Allows user to specify seed used to initialize random number generator (default = determined from current time) and whether each process should print the seed value it uses (default = don't print).",
+ 'pulse': ' The total length of the pulse (in s) if the Pulsed material is used, and the proportion of it that is ON.',
+ 'lattice': ' The length of the lattice side in nm.',
+ 'impurityRadius': ' The radius of the main impurity (He or Xe) in nm.',
+ 'biasFactor': ' This option allows the user to set the bias factor reflecting the fact that interstitial clusters have a larger surrounding strain field.',
+ 'hydrogenFactor': ' The factor between the size of He and H.',
+ 'xenonDiffusivity': ' The diffusion coefficient for xenon in nm2 s-1.',
+ 'fissionYield': ' The number of xenon created for each fission (default is 0.25).',
+ 'heVRatio': ' The number of He atoms allowed per V in a bubble.',
+ 'migrationThreshold': ' Set a limit on the migration energy above which the diffusion will be ignored.'}
 class XolotlFlux():    
     @classinstancemethod
     def generate_particle_flux(self,time, func, **kwargs):
@@ -96,7 +134,7 @@ class XolotlTridyn():
 class XolotlInput(XolotlFlux, XolotlTridyn):
     def __init__(self, paramdef_file='/fusion/projects/boundary/guterlj/parameters_def.txt'):
        super(XolotlInput, self).__init__()
-       self.def_params = self.read_paramdef(paramdef_file)
+       self.def_params = param_def #self.read_paramdef(paramdef_file)
        print('hello')
        self.input_files = {}
        self.input_params = {}
